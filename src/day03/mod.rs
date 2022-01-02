202d9f6f445caf42ecab2dfee00d75fb783781d0
manunio <max.manu.nair@gmail.com>
2022-01-02 08:57:54 +0530

    fixed day 03 - part 1

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -2,17 +2,14 @@ use crate::utils::read_file;
 
 pub fn day03() {
     // get lines from file and parse to vec of string.
-    // let lines = read_file("inputs/day03.txt");
+    let lines = read_file("inputs/day03.txt");
 
-    // parse the collected vec of string to vec of int
-    // let nums: Vec<i32> = lines.iter().map(|num| num.parse().unwrap()).collect();
-
-    println!("Day 03 - Part 1: {}", 1);
-    println!("Day 03 - Part 2: {}", 2);
+    println!("Day 03 - Part 1: {}", part1(&lines));
+    println!("Day 03 - Part 2: {}", "x");
 }
 
 fn part1(lines: &[String]) -> usize {
-    let bit_width = lines[0].len() - 1; // -1 for new line
+    let bit_width = lines[0].len();
     let mut count = vec![0; bit_width];
     let max = lines.len() / 2;
     for line in lines.iter().map(|s| s.chars()) {
@@ -27,8 +24,8 @@ fn part1(lines: &[String]) -> usize {
         gamma = gamma << 1;
         gamma |= (*value > max) as usize;
     }
-    println!("gamma {}", gamma);
-    0
+    let epsilon = ( ( 1 << bit_width ) - 1 ) ^ gamma;
+    epsilon * gamma
 }
 
 #[cfg(test)]
@@ -38,18 +35,18 @@ mod tests {
     #[test]
     fn test_day03() {
         let lines = vec![
-            "00100\n".to_string(),
-            "11110\n".to_string(),
-            "10110\n".to_string(),
-            "10111\n".to_string(),
-            "10101\n".to_string(),
-            "01111\n".to_string(),
-            "00111\n".to_string(),
-            "11100\n".to_string(),
-            "10000\n".to_string(),
-            "11001\n".to_string(),
-            "00010\n".to_string(),
-            "01010\n".to_string(),
+            "00100".to_string(),
+            "11110".to_string(),
+            "10110".to_string(),
+            "10111".to_string(),
+            "10101".to_string(),
+            "01111".to_string(),
+            "00111".to_string(),
+            "11100".to_string(),
+            "10000".to_string(),
+            "11001".to_string(),
+            "00010".to_string(),
+            "01010".to_string(),
         ];
         assert_eq!(part1(&lines), 198);
     }
